d287ae1cfc1c34ba2d542922182a1c1caa76156f
ashut0shj <116025276+ashut0shj@users.noreply.github.com>
2023-01-11 03:12:15 +0530

    aloo puri

---
+++ pattern print/welcome_mat.py
@@ -0,0 +1,20 @@
+#welcome footmat
+
+def welcome_pat():
+    #N=input().split()
+    #n,m=int(N[0]),int(N[1])
+    n=(int(input("enter the size : ")))
+    m=n*3
+    x=(n-1)//2
+    for i in range (x):
+        r=(i*2)+1
+        t=((m)//2)-((r*3)//2)
+        print(("-"*t)+ r*".|." + ("-"*t))
+    l=(m-7)//2
+    print(("-"*l)+"WELCOME"+("-"*l))
+    for ii in range (x):
+        i=x-ii-1
+        r=(i*2)+1
+        t=((m)//2)-((r*3)//2)
+        print(("-"*t)+ r*".|." + ("-"*t))
+welcome_pat()
